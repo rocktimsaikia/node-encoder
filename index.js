f6353b0ef4dc8ad8a03afacefb80a8d131c15d91
rocktimsaikia <srocktim61@gmail.com>
2020-08-25 17:53:00 +0530

    chore: add jsdoc

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 'use strict';
 
+/**
+ * Checks if the input is valid string
+ * @param {String} input | utf-8 or base64 string
+ */
 const _typeValidation = input => {
 	if (typeof input !== 'string') {
 		throw new TypeError(`Expected a string, got ${typeof input}`);
 	}
 };
 
+/**
+ * Returns base64 string
+ * @param {string} input | utf-8 string
+ */
 const encode = input => {
 	_typeValidation(input);
 
@@ -14,6 +22,10 @@ const encode = input => {
 	return encoded;
 };
 
+/**
+ * Returns utf-8 string
+ * @param {string} input | base64 string
+ */
 const decode = input => {
 	_typeValidation(input);
 
