5595ddf33c9e335f0ed3cfc727a67a0cc92c177c
Rocktim Saikia <srocktim61@gmail.com>
2020-09-04 20:37:38 +0530

    v2.0.2 :sparkles: (#2)
    add type definitions

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Converts utf-8 string to base64
+ * @param input | utf-8 input string
+ */
+export function encode(input: string): string;
+
+/**
+ * Converts base-64 string to utf-8
+ * @param input | base-64 input string
+ */
+export function decode(input: string): string;
